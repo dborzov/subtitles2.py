df79c9a37798ebadaba8b5dfff2974dfb154a05e
Dima Borzov <tihoutrom@gmail.com>
2013-05-26 01:48:57 -0700

    search actually working, whoah! :)

--- app/application/static/js/custom.js
+++ app/application/static/js/custom.js
@@ -10,15 +10,25 @@ $(document).ready(function(){
             type:'POST',
             headers: {"Content-type": "application/json"},
             success:function(data) {
-                console.log('Received response: '+data);
+                console.log('Received response! ');
                 window.theresponse = data;
+                eval('dicty='+data+';');
                 var view = {
                     flaky_in: thecommand,
                     flaky_out: window.theresponse
                 };
 
-                dima_paste = Mustache.render('<div class="row-fluid"><div class="span2">&nbsp;</div><div class="span8 flaky-query"><p class="lead text-center"> {{flaky_in}} </div><div class="span2">&nbsp;</div></div><div class="row-fluid"><div class="span2">&nbsp;</div><div class="span8 flaky-response"><p class="lead text-center"> {{flaky_out}} </div><div class="span2">&nbsp;</div></div>', view);
-                $("#dimasscreen").prepend(dima_paste);
+                $("#dimasscreen").empty()
+                for (var i=0;i < dicty.length;i++){
+                    console.log('Hello: '+dicty[i]["line"]);
+                    if (i%2 == 0) {
+                        dima_paste = '<div class="row-fluid"><div class="span2">&nbsp;</div><div class="span8"><div class="span3  flaky-movie"><span class="lead text-center movietitle">'+ dicty[i]["movie"]+'</span></div><div class="span9  flaky-query"><p class="lead text-center">'+ dicty[i]["line"] +'</div></div><div class="span2">&nbsp;</div></div><div class="row-fluid"><div class="span2">&nbsp;</div><div class="span8 flaky-response"><p class="lead text-center">'+ dicty[i]["context"]+'</div><div class="span2">&nbsp;</div></div>';
+                    } else {
+                        dima_paste = '<div class="row-fluid"><div class="span2">&nbsp;</div><div class="span8">'+'<div class="span9  flaky-query"><p class="lead text-center">'+ dicty[i]["line"] +'</div>'+'<div class="span3  flaky-movie"><span class="lead text-center movietitle">'+ dicty[i]["movie"]+'</span></div>'+'</div><div class="span2">&nbsp;</div></div><div class="row-fluid"><div class="span2">&nbsp;</div><div class="span8 flaky-response"><p class="lead text-center">'+ dicty[i]["context"]+'</div><div class="span2">&nbsp;</div></div>';
+                    }
+
+                    $("#dimasscreen").prepend(dima_paste);
+                }
                 }
         });
 
--- app/application/views.py
+++ app/application/views.py
@@ -1,14 +1,15 @@
 import json
 from google.appengine.api import users
+from google.appengine.ext import ndb
 from google.appengine.runtime.apiproxy_errors import CapabilityDisabledError
 
 from flask import request, render_template, flash, url_for, redirect
 
 from flask_cache import Cache
-
+import texter
 from application import app
 from decorators import login_required, admin_required
-from models import DictionaryWord
+from models import DictionaryWord, Mention
 import re
 
 
@@ -16,15 +17,35 @@ import re
 cache = Cache(app)
 
 
+def parser(word):
+    """ individual word into array of dicts
+    of quotes """
+    dict_match = DictionaryWord.query(
+            DictionaryWord.word == word).fetch()
+    if dict_match:
+        mentions = Mention.query(Mention.word == dict_match[0].key).fetch()
+        yeild_text = []
+        for mention in mentions:
+            quote = mention.quote.get()
+            quote_dict = {'line':quote.line,
+                        'context':quote.context,
+                        'movie':quote.movie
+                        }
+            yeild_text.append(quote_dict)
+        return yeild_text
+    else:
+        return []
+
 
 def say_hello():
     query = json.loads(request.data)
-    dict_match = DictionaryWord.query(
-            DictionaryWord.word == query['query']).fetch()
-    if dict_match:
-        return dict_match[0].word + ' ,freq:' + str(dict_match[0].frequency)
-    else:
-        return ", ".join([query['query']])
+    words = texter.words_from_string(query["query"])
+    out = []
+    for word in words:
+        out += parser(word)
+    if not out:
+        out = [{'line': '', 'context': '<h1>:/</h1>Nothing found', 'movie': ''}]
+    return json.dumps(out)
 
 
 
