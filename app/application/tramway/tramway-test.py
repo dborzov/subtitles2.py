38d994af53959d909fecd99ea3c59f569dc6b5f5
Dima Borzov <tihoutrom@gmail.com>
2013-05-30 09:02:37 -0700

    Moved AppEngine instance to a separate repo

--- app/application/tramway/tramway-test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import subtitles2flashcards as testee
-
-class TestFindQoutes(unittest.TestCase):
-    def testAdd(self):
-        self.assertEqual([], testee.QuoteForAWord('banana', 'How much is the fish?'))
-
-unittest.main()
--- app/run.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.join(os.path.abspath('.'), 'lib'))
-import application
-
